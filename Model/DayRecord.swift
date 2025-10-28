2baa7b6b7757f99d639298cf0684592c4d09ee8c
realdongchimi <127817494+realdongchimi@users.noreply.github.com>
2025-10-28 17:47:08 +0900

    Add comments to DayRecord properties

--- Model/DayRecord.swift
+++ Model/DayRecord.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct DayRecord: Identifiable, Codable, Equatable {
     var id: String { dateKey }      // "YYYY-MM-DD"
-    let dateKey: String
-    var totalML: Int
-    var entries: [DrinkEntry]
+    let dateKey: String    // 시간 지정하기 위한 날짜키
+    var totalML: Int    // 해당 날짜 중 섭취량
+    var entries: [DrinkEntry]    // 시간별 기록 리스트
 }
