f412f61ac4ada8d4d1228eefd1491b6298fcd878
realdongchimi <127817494+realdongchimi@users.noreply.github.com>
2025-10-28 20:06:20 +0900

    Add progress variable to display percentage
    Add progress variable to PercentBlock for percentage display

--- View/PercentBlock.swift
+++ View/PercentBlock.swift
@@ -1,7 +1,10 @@
+// 물 섭취를 어느정도 했는지 퍼센트를 알려줌
 import SwiftUI
 
 struct PercentBlock: View {
+    // 진행률 (0.0~1.0)
     var progress: Double
+    // 퍼센트로 변환  (0~100)
     var body: some View {
         Text("\(Int(progress * 100))%")
             .font(.system(size: 60, weight: .heavy))
