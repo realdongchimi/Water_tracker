12accfb2a910e968a1cf5afdc7329cc3585b5e46
realdongchimi <127817494+realdongchimi@users.noreply.github.com>
2025-10-28 17:48:05 +0900

    Add comments to DrinkEntry properties
    Added comments to properties for clarity.

--- Model/DrinkEntry.swift
+++ Model/DrinkEntry.swift
@@ -1,9 +1,10 @@
 import Foundation
 
+// 시간별 섭취 기록
 struct DrinkEntry: Identifiable, Codable, Equatable {
     let id: UUID
-    var time: Date
-    var amount: Int
+    var time: Date    // 기록 시각
+    var amount: Int    // 섭취량
 
     init(id: UUID = UUID(), time: Date = Date(), amount: Int) {
         self.id = id
